6332f696dc7d3155b045e1a103ce47e917a5877b
g24force <102439759+g24force@users.noreply.github.com>
2024-01-19 12:53:53 +0000

    Solution to js exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let string = '';
+    for (i = 1; i <= times; i++) {
+        string += word;
+    }
+    if (times < 0) {
+        return string = 'ERROR';
+    } else {
+        return string;
+    }
 };
 
 // Do not edit below this line
