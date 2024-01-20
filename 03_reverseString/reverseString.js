eebf8948e5b977b6d9e8e9485bb3f8ce6eebff34
g24force <102439759+g24force@users.noreply.github.com>
2024-01-20 02:54:39 +0000

    Solution to js exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const letters = [];
+    let i = 0;
+    while (i < string.length) {
+        letters.push(string.slice(i, i + 1));
+        i++;
+    }
+    let reversedLetters = letters.reverse();
+    return letters.join("");
 };
 
 // Do not edit below this line
