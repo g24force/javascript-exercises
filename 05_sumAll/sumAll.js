a8746079f10ad3955be10e0b201d73d094686c43
g24force <102439759+g24force@users.noreply.github.com>
2024-01-22 00:11:04 +0000

    Solution to js exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let finalSum = 0;
+    if (Number.isInteger(num1) && Number.isInteger(num2) && num1 >= 0 && num2 >= 0) {
+        if (num2 > num1) {
+            for (i = num1; i <= num2; i++) {
+                finalSum += i;
+            }
+        } else if (num1 > num2) {
+            for (i = num2; i <= num1; i++) {
+                finalSum += i;
+        }}
+        return finalSum;
+    } else {
+        return "ERROR";
+    }
 
 };
 
