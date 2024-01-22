59ddd501c20e0bd2ab15bffcc1a1ca27a5f302e9
g24force <102439759+g24force@users.noreply.github.com>
2024-01-22 00:32:38 +0000

    Solution to js exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0 && (year % 400 === 0 || year % 100 !== 0)) {
+        return true;
+    } else { return false; }
 };
+
+//divisible by 4
+//not divisible by 100
+//divisible by 400
+
 
 // Do not edit below this line
 module.exports = leapYears;
