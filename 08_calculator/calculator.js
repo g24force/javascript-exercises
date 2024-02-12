825f64e163ba8e634dbb7debfa2e7f51ec1a7fe6
g24force <102439759+g24force@users.noreply.github.com>
2024-02-12 14:35:24 +0000

    Solution to js exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let total = 0;
+  for (let i = 0; i < a.length; i++) {
+    total += a[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let total = 1;
+  for (let i = 0; i < a.length; i++) {
+    total = total * a[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) {
+    return 1;
+  } else {
+    let total = 1;
+    for (let i = 1; i <= n; i++) {
+      total = total * i;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
