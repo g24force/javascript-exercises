54b243477ffa36aad78b8bd583564546a790f13f
g24force <102439759+g24force@users.noreply.github.com>
2024-02-14 16:16:55 +0000

    Solution to js exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+/* const palindromes = function (string) {
+    let splitString = string.split("");
+    let removedPunct = splitString.filter(char => char.replace(/[^\w\s]/g, ""));
+    let reverseArray = removedPunct.reverse();
+    let joinArray = reverseArray.join("").replace(/[^\w\s]/g, "");
+    console.log(joinArray)
+    if (joinArray === string) {
+        return true;
+    } else return false;
 };
+
+console.log(palindromes("racecar!")) */
+
+const palindromes = function (string) {
+    const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return processedString.split("").reverse().join("") == processedString;
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
